9929352011d44029c2c52eb16ab76cc8a13464f2
rasto <rasto.skuba@gmail.com>
2025-01-09 14:15:09 +0100

    docker

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,17 +1,14 @@
+import './assets/main.css'
+
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
-import App from '@/App.vue'
-import { registerPlugins } from '@core/utils/plugins'
+import App from './App.vue'
+import router from './router'
 
-// Styles
-import '@core/scss/template/index.scss'
-import '@styles/styles.scss'
-
-// Create vue app
 const app = createApp(App)
 
-// Register plugins
-registerPlugins(app)
+app.use(createPinia())
+app.use(router)
 
-// Mount vue app
 app.mount('#app')
