a16c906b33c9c9d37ed6f7b849c90ea5f927e509
rasto <rasto.skuba@gmail.com>
2025-01-12 08:43:50 +0100

    update

--- frontend-admin/vitest.config.ts
+++ frontend-admin/vitest.config.ts
@@ -1,3 +1,5 @@
+// <reference types="node" />
+
 import { fileURLToPath } from 'node:url'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
